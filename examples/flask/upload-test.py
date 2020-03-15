b32f73e3131f59ca114cffd66c6c45ae1dd749e8
Ali Momen Sani <ali.momensany@gmail.com>
2020-03-15 17:58:13 +0330

    test MultiFileTarget

--- examples/flask/upload-test.py
+++ examples/flask/upload-test.py
@@ -10,6 +10,41 @@ from flask import Flask, request
 from streaming_form_data import StreamingFormDataParser
 from streaming_form_data.targets import FileTarget
 
+from streaming_form_data.targets import BaseTarget
+
+
+class MultiFileTarget(BaseTarget):
+    def __init__(self, dirpath, allow_overwrite=True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.dirpath = dirpath
+        print(dirpath)
+        os.mkdir(dirpath)
+
+        self._mode = 'wb' if allow_overwrite else 'xb'
+        self._fd = None
+        self.count = 0
+        self.filenames = []
+        self.content_types = []
+
+    def on_start(self):
+        print(f'on_start {self.multipart_filename}')
+        self.count += 1
+        self._fd = open(
+            os.path.join(self.dirpath, f'{self.count}'), self._mode
+        )
+
+    def on_data_received(self, chunk: bytes):
+        self._fd.write(chunk)
+
+    def on_finish(self):
+        print(
+            f'on_finish {self.multipart_filename} {self.multipart_content_type}'
+        )
+        self.filenames.append(self.multipart_filename)
+        self.content_types.append(self.multipart_content_type)
+        self._fd.close()
+
 
 app = Flask(__name__)
 
@@ -23,7 +58,7 @@ page = dedent(
     <body>
         <h1>Upload new File</h1>
         <form method="post" enctype="multipart/form-data" id="upload-file">
-          <input type="file" name="file">
+          <input type="file" name="file" multiple>
           <input type="submit" value="Upload">
         </form>
     </body>
@@ -34,7 +69,7 @@ page = dedent(
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        file_ = FileTarget(os.path.join(tempfile.gettempdir(), 'test'))
+        file_ = MultiFileTarget(os.path.join(tempfile.gettempdir(), 'test'))
 
         parser = StreamingFormDataParser(headers=request.headers)
 
